bcc650d2c15f8a6a2420a692ae24647607442d64
Vlad <vladsaf@rambler.ru>
2016-09-06 14:27:38 +0300

    added  preview file and index.psd and correct  file main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,74 @@
-$(document).ready(function () {
-    (function () {
+"use strict";
 
-        var
-            nav = $('.header__nav');
+var dropDown = (function () {
 
-        $('.nav__link_popup, .nav__popup').on('mouseenter', function () {
+    var nav = $(".header__nav");
+    var init = function () {
+        _active();
+    };
+    var _active = function () {
+        $(".nav__link_popup, .nav__popup").on("mouseenter", function () {
 
-            nav.addClass('active');
+            nav.addClass("active");
 
-        }).on('mouseleave', function () {
+        }).on("mouseleave", function () {
 
-            nav.removeClass('active');
+            nav.removeClass("active");
         });
-    }());
+    };
+    return {
+        init: init
+    };
+}());
+
+var bigSlider = (function () {
+
+    var init = function () {
+        _bxSlider();
+    };
+    var _bxSlider = function () {
+        $(".bxslider").bxSlider();
+    };
+    return {
+        init: init
+    };
+}());
+
+var smallSlider = (function () {
+
+    var init = function () {
+        _smallSlider();
+    };
+    var _smallSlider = function () {
+        $(".clients__slider").bxSlider({
+            minSlides: 3,
+            maxSlides: 6,
+            slideWidth: 170,
+            slideMargin: 30,
+            pager: false
+        });
+    };
+    return {
+        init: init
+    };
+}());
+
+$(document).ready(function () {
+
+    if ($(".clients__slider").length) {
+        smallSlider.init();
+    }
+
+    if ($(".bxslider").length) {
+        bigSlider.init();
+    }
+
+    if ($(".header__nav").length) {
+        dropDown.init();
+    }
 });
 
-$(document).ready(function(){
-    $('.bxslider').bxSlider();
-});
 
-$(document).ready(function(){
-    $('.clients__slider').bxSlider({
-        minSlides: 3,
-        maxSlides: 6,
-        slideWidth: 170,
-        slideMargin: 30,
-        pager: false
-    });
-});
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
-'use strict'
+"use strict";
 
-var gulp = require('gulp'),
-    gulpif = require('gulp-if'),
-    del = require('del'),
-    plumber = require('gulp-plumber'),
+var gulp = require("gulp"),
+    gulpif = require("gulp-if"),
+    del = require("del"),
+    plumber = require("gulp-plumber"),
     notify = require("gulp-notify"),
-    browserSync = require('browser-sync').create(),
+    browserSync = require("browser-sync").create(),
     reload = browserSync.reload,
-    uglify = require('gulp-uglify'),
-    cleanCSS = require('gulp-clean-css'),
-    csscomb = require('gulp-csscomb'),
-    useref = require('gulp-useref'),
-    wiredep = require('wiredep').stream,
-    sourcemaps = require('gulp-sourcemaps'),
-    stylus = require('gulp-stylus'),
-    nib = require('nib'),
-    sftp = require('gulp-sftp'),
-    pug = require('gulp-pug'),
-    size = require('gulp-size'),
-    changed = require('gulp-changed'),
-    imagemin = require('gulp-imagemin'),
-    prettify = require('gulp-prettify');
+    uglify = require("gulp-uglify"),
+    cleanCSS = require("gulp-clean-css"),
+    csscomb = require("gulp-csscomb"),
+    useref = require("gulp-useref"),
+    wiredep = require("wiredep").stream,
+    sourcemaps = require("gulp-sourcemaps"),
+    stylus = require("gulp-stylus"),
+    nib = require("nib"),
+    sftp = require("gulp-sftp"),
+    pug = require("gulp-pug"),
+    size = require("gulp-size"),
+    changed = require("gulp-changed"),
+    imagemin = require("gulp-imagemin"),
+    prettify = require("gulp-prettify");
 
 // Запуск BROWSERSYNC
-gulp.task('server', ['pug', 'stylus'], function () {
+gulp.task("server", ["pug", "stylus"], function () {
     browserSync.init({
         server: "app",
         port: 9000
@@ -31,120 +31,112 @@ gulp.task('server', ['pug', 'stylus'], function () {
 });
 
 //Добавляет библиотеки BOWER в Pug(Jade)
-gulp.task('bower', function () {
-    return gulp.src('app/templates/common/*.pug')
+gulp.task("bower", function () {
+    return gulp.src("app/templates/common/*.pug")
         .pipe(wiredep({
             ignorePath: /^(\.\.\/)*\.\./
         }))
-        .pipe(gulp.dest('app/templates/common'))
+        .pipe(gulp.dest("app/templates/common"));
 });
 
 // Компиляция Pug(Jade) файлов в HTML
-gulp.task('pug', function () {
-    return gulp.src('app/templates/pages/*.pug')
-        .pipe(changed('app/'))
+gulp.task("pug", function () {
+    return gulp.src("app/templates/pages/*.pug")
+        .pipe(changed("app/"))
         .pipe(plumber({
             errorHandler: notify.onError("Error: <%= error.message %>")
         }))
         .pipe(pug())
         .pipe(prettify({indent_size: 4}))
-        .pipe(gulp.dest('app/'))
+        .pipe(gulp.dest("app/"))
         .pipe(browserSync.stream());
 });
 
 // Компиляция stylus файлов в CSS
-gulp.task('stylus', function () {
-    return gulp.src('app/stylus/**/main.styl')
-        .pipe(changed('app/css'))
+gulp.task("stylus", function () {
+    return gulp.src("app/stylus/**/main.styl")
+        .pipe(changed("app/css"))
     // Не прерывает поток, выводит ошибку
         .pipe(plumber({
             errorHandler: notify.onError("Error: <%= error.message %>")
         }))
         .pipe(sourcemaps.init())
-        .pipe(stylus({ use: nib(),  import: ['nib']}))
+        .pipe(stylus({use: nib(), import: ["nib"]}))
         // Приводит к единообразному виду CSS
         .pipe(csscomb())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('app/css'))
+        .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
 });
 
 // Слежка за файлами
-gulp.task('watch', function () {
-    gulp.watch('app/templates/**/*.*', ['pug']);
-    gulp.watch('app/stylus/**/*.*', ['stylus']);
-    gulp.watch('bower.json', ['bower']);
-    gulp.watch(["app/js/**/*.js",
-        "app/css/**/*.css",
-        "app/**/*.html"
-        ]).on("change", reload);
+gulp.task("watch", function () {
+    gulp.watch("app/templates/**/*.*", ["pug"]);
+    gulp.watch("app/stylus/**/*.*", ["stylus"]);
+    gulp.watch("bower.json", ["bower"]);
+    gulp.watch(["app/js/**/*.js", "app/css/**/*.css", "app/**/*.html"]).on("change", reload);
 });
 
-gulp.task('default', ['server', 'watch']);
+gulp.task("default", ["server", "watch"]);
 
-// Очищает папку 'public'
-gulp.task('clean', function () {
-    return del('public/**/*');
+// Очищает папку "public"
+gulp.task("clean", function () {
+    return del("public/**/*");
 });
 
-// Собирает в папку 'public' картинки
-gulp.task('copy:img', function () {
-    return gulp.src('app/img/**/*')
+// Собирает в папку "public" картинки
+gulp.task("copy:img", function () {
+    return gulp.src("app/img/**/*")
         .pipe(imagemin({
             progressive: true,
             interlaced: true
         }))
-        .pipe(gulp.dest('public/img'));
+        .pipe(gulp.dest("public/img"));
 
 });
 
-// Собирает в папку 'public' шрифты
-gulp.task('copy:fonts', function () {
-    gulp.src('app/fonts/*.*')
-        .pipe(gulp.dest('public/fonts/'))
+// Собирает в папку "public" шрифты
+gulp.task("copy:fonts", function () {
+    gulp.src("app/fonts/*.*")
+        .pipe(gulp.dest("public/fonts/"));
 });
 
-// Собирает в папку 'public' остальные файлы(например favicon.ico)
-gulp.task('extras', function () {
-    return gulp.src(['app/*.*', '!app/*.html'])
-        .pipe(gulp.dest('public'))
+// Собирает в папку "public" остальные файлы(например favicon.ico)
+gulp.task("extras", function () {
+    return gulp.src(["app/*.*", "!app/*.html"])
+        .pipe(gulp.dest("public"));
 });
 
 // Сборка и минификация всех js, css в папку public
-gulp.task('useref', function () {
-    return gulp.src('app/*.html')
+gulp.task("useref", function () {
+    return gulp.src("app/*.html")
     //Собирает все js и css в файлы main and vendor
         .pipe(useref())
         //Минифицирует js
-        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif("*.js", uglify()))
         //Минифицирует css
-        .pipe(gulpif('*.css', cleanCSS({compatibility: 'ie8'})))
-        .pipe(gulp.dest('public'));
+        .pipe(gulpif("*.css", cleanCSS({compatibility: "ie8"})))
+        .pipe(gulp.dest("public"));
 });
 
 // Сборка папки public и вывод размера этой папки
-gulp.task('public', ['useref', 'copy:img', 'copy:fonts', 'extras'], function () {
-    return gulp.src('public/**/*')
-        .pipe(size({title: 'build'}))
+gulp.task("public", ["useref", "copy:img", "copy:fonts", "extras"], function () {
+    return gulp.src("public/**/*")
+        .pipe(size({title: "build"}));
 });
 
 // Сборка папки public только после ее очистки и компиляции Pug(Jade) Stylus файлов
-gulp.task('build', ['clean', 'pug', 'stylus'], function () {
-    gulp.start('public');
+gulp.task("build", ["clean", "pug", "stylus"], function () {
+    gulp.start("public");
 });
 
 // Отправка папки public на сервер
-gulp.task('sftp', function () {
-    return gulp.src('public/**/*')
+gulp.task("sftp", function () {
+    return gulp.src("public/**/*")
         .pipe(sftp({
-            host: 'website.com',
-            user: 'johndoe',
-            pass: '1234',
-            remotePath: 'httn/hhh/'
+            host: "website.com",
+            user: "johndoe",
+            pass: "1234",
+            remotePath: "httn/hhh/"
         }));
 });
-
-
-
-
-
