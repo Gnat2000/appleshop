f56d48616c98cdd5171efafe8f3cb30ad464baac
Vlad <vladsaf@rambler.ru>
2016-09-18 19:06:13 +0300

    Refactoring pug and stylus files

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,23 @@
 "use strict";
 
-var dropDown = (function () {
+$(document).ready(function () {
 
-    var nav = $(".header__nav");
-    var init = function () {
-        _active();
-    };
-    var _active = function () {
+    if ($(".clients__slider").length) {
+        $(".clients__slider").bxSlider({
+            minSlides: 3,
+            maxSlides: 6,
+            slideWidth: 170,
+            slideMargin: 30,
+            pager: false
+        });
+    }
+
+    if ($(".slider__list").length) {
+        $(".slider__list").bxSlider();
+    }
+
+    if ($(".nav").length) {
+        var nav = $(".nav");
         $(".nav__link_popup, .nav__popup").on("mouseenter", function () {
 
             nav.addClass("active");
@@ -15,56 +26,6 @@ var dropDown = (function () {
 
             nav.removeClass("active");
         });
-    };
-    return {
-        init: init
-    };
-}());
-
-var bigSlider = (function () {
-
-    var init = function () {
-        _bxSlider();
-    };
-    var _bxSlider = function () {
-        $(".bxslider").bxSlider();
-    };
-    return {
-        init: init
-    };
-}());
-
-var smallSlider = (function () {
-
-    var init = function () {
-        _smallSlider();
-    };
-    var _smallSlider = function () {
-        $(".clients__slider").bxSlider({
-            minSlides: 3,
-            maxSlides: 6,
-            slideWidth: 170,
-            slideMargin: 30,
-            pager: false
-        });
-    };
-    return {
-        init: init
-    };
-}());
-
-$(document).ready(function () {
-
-    if ($(".clients__slider").length) {
-        smallSlider.init();
-    }
-
-    if ($(".bxslider").length) {
-        bigSlider.init();
-    }
-
-    if ($(".header__nav").length) {
-        dropDown.init();
     }
 });
 
